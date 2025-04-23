763b256aaf12155c00245a35406166f603cbc64c
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-04-23 16:47:02 +0200

    Header style, fix typo

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-header",
     standalone: true,  // true by default in Angular 19 or higher
-    templateUrl: "./header.component.html"
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.css",
 })
 export class HeaderComponent {}
