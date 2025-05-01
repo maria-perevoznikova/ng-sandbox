d5415d8ecd21372b86490c2f255e2797a40d186a
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-05-01 16:17:01 +0200

    Pick a new random user on button click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  currentUser = DUMMY_USERS[randomIndex];
+  currentUser = signal(DUMMY_USERS[0]);
+  currentAvatarPath = computed(() => {
+    return `assets/users/${this.currentUser().avatar}`;
+  });
+
+  // get currentAvatarPath(): string {
+  //   return `assets/users/${this.currentUser().avatar}`;
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.currentUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
