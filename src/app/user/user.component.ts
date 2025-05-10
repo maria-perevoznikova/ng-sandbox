4aa663ac5f34a795a058bc05189d62de9448f9a2
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-05-10 14:04:17 +0200

    Use input signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
 
 @Component({
@@ -8,14 +8,23 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // 'old' style input
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get avatarPath(): string {
-    return `assets/users/${this.avatar}`;
-  }
+  // get avatarPath(): string {
+  //   return `assets/users/${this.avatar}`;
+  // }
+
+  // signal inputs
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  avatarPath = computed(
+    () => `assets/users/${this.avatar()}`
+  );
 
   onSelectUser() {
-    console.log('User selected:', this.name);
+    console.log('User selected:', this.name());
   }
 }
