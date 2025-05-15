a422c2e5c0ee30ad67227e669c4509f916e90b98
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-05-15 15:56:51 +0200

    Add task component and styles

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,16 @@
 import { Component, input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  add() {
+    throw new Error('Method not implemented.');
+  }
   userId = input.required<string>();
   userName = input.required<string>();
 }
