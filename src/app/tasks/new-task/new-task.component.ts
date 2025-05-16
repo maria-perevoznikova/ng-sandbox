876351bcaf19c6e525c6a0515c10d5d87a8087b2
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-05-16 11:35:04 +0200

    Add 'new task' component

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  userId = input.required<string>();
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, computed, signal } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -13,20 +14,20 @@ export class TasksComponent {
 
   userId = input.required<string>();
   userName = input.required<string>();
+  isAddingTask = signal(false);
 
   selectedUserTasks = computed(() => {
     return this.allUsersTasks().filter((task) => task.userId === this.userId());
   });
 
-  add() {
-    throw new Error('Method not implemented.');
+  onStartAddingTask() {
+    this.isAddingTask.set(true);
+    console.log('Adding a new task for user:', this.userId());
   }
 
   onCompleteTask(id: string) {
     // this.allUsersTasks.set(this.allUsersTasks().filter((task) => task.id !== id));
     this.allUsersTasks.update(v => v.filter((task) => task.id !== id));
-
     console.log('Task completed:', id);
-    console.log('Remaining tasks:', this.allUsersTasks);
   }
 }
