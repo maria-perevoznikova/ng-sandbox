2b99bbf77ea69349aa02e2478c7dc5b41a7faeda
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-05-25 16:47:27 +0200

    Use local storage to store tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,20 +6,33 @@ import { NewTask } from './new-task/new-task.model';
 export class TasksService {
   private tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
   addTask(task: NewTask) {
     this.tasks = [...this.tasks, { ...task, id: this.generateNewId() }];
+    this.saveTasks();
   }
 
   deleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
   }
 
   private generateNewId(): string {
     // todo improve
     return new Date().getTime().toString();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
