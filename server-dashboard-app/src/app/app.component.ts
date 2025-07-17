c66f16bab88fb4ed961a1d5a906c2b4e330efd1d
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-07-17 16:55:27 +0200

    Split app into subcomponents

--- server-dashboard-app/src/app/app.component.ts
+++ server-dashboard-app/src/app/app.component.ts
@@ -1,41 +1,14 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ServerStatusComponent } from './server-status/server-status.component';
+import { TrafficComponent } from './traffic/traffic.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [NavigationComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
 }
