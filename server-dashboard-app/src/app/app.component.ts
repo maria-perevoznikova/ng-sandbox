398cb95a170e18e4d7629210f4e1be29362a8e0f
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-07-17 17:02:53 +0200

    Move & rename

--- server-dashboard-app/src/app/app.component.ts
+++ server-dashboard-app/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component';
-import { ServerStatusComponent } from './server-status/server-status.component';
-import { TrafficComponent } from './traffic/traffic.component';
-import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import {TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+  imports: [NavigationComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
