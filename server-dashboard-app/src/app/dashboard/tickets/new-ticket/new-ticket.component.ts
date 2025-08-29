29182fba121eaaca0130cf774c67fe92c4e89039
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-08-29 13:27:16 +0200

    control component

--- server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ButtonComponent} from "../../../shared/button/button.component";
+import {ControlComponent} from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     FormsModule,
-    ButtonComponent
+    ButtonComponent,
+    ControlComponent
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
