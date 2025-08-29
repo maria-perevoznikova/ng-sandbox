5bd4bb0b669352a864cfcec3e939ccd88f3c56d1
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-08-29 12:28:33 +0200

    new-ticket & ticket components

---
+++ server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css'
+})
+export class NewTicketComponent {
+
+  onCreate() {
+
+  }
+}
