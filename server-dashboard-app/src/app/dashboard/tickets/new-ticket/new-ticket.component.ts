5618ac6ff12932002eff8610f17229f79e000831
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-09-04 17:13:40 +0200

    viewChild function example

--- server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+// import {ElementRef, viewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ButtonComponent} from "../../../shared/button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
@@ -17,11 +18,12 @@ import {ControlComponent} from "../../../shared/control/control.component";
 export class NewTicketComponent {
 
   // viewChild function to access the form element
-  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // private formSignal = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, content: string, form: HTMLFormElement) {
     console.log('Ticket title: ', title);
     console.log('Ticket content: ', content);
     form.reset();
+    // this.formSignal().nativeElement.reset();
   }
 }
