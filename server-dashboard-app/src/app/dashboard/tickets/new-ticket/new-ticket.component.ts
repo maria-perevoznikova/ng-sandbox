aad826dc13fab3b121d827c3df729edb0c80a49d
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-09-10 17:27:09 +0200

    use two-way binding

--- server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ server-dashboard-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import {Component, output} from '@angular/core';
+import {Component, input, output, signal} from '@angular/core';
 // import {ElementRef, viewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ButtonComponent} from "../../../shared/button/button.component";
@@ -20,11 +20,20 @@ export class NewTicketComponent {
   // viewChild function to access the form element
   // private formSignal = viewChild.required<ElementRef<HTMLFormElement>>('form');
   add = output<{title: string, content: string}>();
+  enteredTitle = signal<string>('');
+  enteredRequestText = signal<string>('');
 
   onSubmit(title: string, content: string, form: HTMLFormElement) {
-    console.log('Submit ticket', title);
-    this.add.emit({title, content});
-    form.reset();
+    console.log('Submit ticket', title, content);
+
+    // use two-way binding with signals
+    this.add.emit({title: this.enteredTitle(), content: this.enteredRequestText()});
+    this.enteredTitle.set('');
+    this.enteredRequestText.set('');
+
+    // use template reference variable to access the form element
+    // this.add.emit({title, content});
+    // form.reset();
     // this.formSignal().nativeElement.reset();
   }
 }
