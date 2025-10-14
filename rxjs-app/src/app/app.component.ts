a8b2a5322c9890bb40beec4af7cc73f263ffd052
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-10-14 18:28:40 +0200

    add custom observable

--- rxjs-app/src/app/app.component.ts
+++ rxjs-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
-import {interval, map} from 'rxjs';
+import {Component, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
+import {interval, map, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,27 @@ import {interval, map} from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  customInterval$ = new Observable(subscriber => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+
+      subscriber.next({message: 'Emitting from custom interval'});
+      timesExecuted++;
+    }, 2000);
+  });
   private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    effect(() => {
+      console.log('Click count:', this.clickCount());
+    });
+  }
 
   ngOnInit(): void {
     const subscription = interval(1000)
@@ -16,8 +36,19 @@ export class AppComponent implements OnInit {
         console.log(count);
       });
 
+    const customSubscription = this.customInterval$.subscribe({
+      next: (data) => console.log('Custom interval subscription:', data),
+      complete: () => console.log('Custom interval completed'),
+      error: (error) => console.error('Custom interval error:', error)
+    });
+
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
+      customSubscription.unsubscribe();
     });
+  }
+
+  onClick() {
+    this.clickCount.update(count => count + 1);
   }
 }
