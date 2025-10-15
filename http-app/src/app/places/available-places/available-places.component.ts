7c159fa5a3e271022395a0d9e87a62bff18d46fe
Maria Schoenholzer <maria.schoenholzer@gmail.com>
2025-10-15 16:15:21 +0200

    add loading state for available places fetching

--- http-app/src/app/places/available-places/available-places.component.ts
+++ http-app/src/app/places/available-places/available-places.component.ts
@@ -1,10 +1,10 @@
 import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {Place} from '../place.model';
+import {PlacesComponent} from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
 import {HttpClient} from "@angular/common/http";
-import { BASE_URL } from '../../config';
+import {BASE_URL} from '../../config';
 
 @Component({
   selector: 'app-available-places',
@@ -14,14 +14,17 @@ import { BASE_URL } from '../../config';
   imports: [PlacesComponent, PlacesContainerComponent],
 })
 export class AvailablePlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
+  places = signal<Place[]>([]);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{places: Place[]}>(`${BASE_URL}/places`).subscribe({
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>(`${BASE_URL}/places`).subscribe({
       next: (data) => this.places.set(data.places),
-    })
+      complete: () => this.isFetching.set(false)
+    });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
